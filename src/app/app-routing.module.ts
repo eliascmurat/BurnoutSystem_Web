c532044a8553eb62582f570e6021a6d128019e1b
eliascmurat <elias-murat@outlook.com>
2023-08-28 21:14:10 -0300

    refactor: rename directors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,27 +10,27 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () =>
-      import('./pages/home/home.module').then((m) => m.HomeModule),
+      import('./modules/home/home.module').then((m) => m.HomeModule),
   },
   {
     path: 'press-release',
     loadChildren: () =>
-      import('./pages/press-release/press-release.module').then((m) => m.PressReleaseModule),
+      import('./modules/press-release/press-release.module').then((m) => m.PressReleaseModule),
   },
   {
     path: 'contact',
     loadChildren: () =>
-      import('./pages/contact/contact.module').then((m) => m.ContactModule),
+      import('./modules/contact/contact.module').then((m) => m.ContactModule),
   },
   {
     path: 'thanks',
     loadChildren: () =>
-      import('./pages/thanks/thanks.module').then((m) => m.ThanksModule),
+      import('./modules/thanks/thanks.module').then((m) => m.ThanksModule),
   },
   {
     path: 'not-found',
     loadChildren: () =>
-      import('./pages/not-found/not-found.module').then((m) => m.NotFoundModule),
+      import('./modules/not-found/not-found.module').then((m) => m.NotFoundModule),
   },
   {
     path: '**',
