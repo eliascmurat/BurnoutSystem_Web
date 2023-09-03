601f6395b8fe9c6ced502a490bf1c91c23db85fc
eliascmurat <elias-murat@outlook.com>
2023-09-03 12:06:14 -0300

    refactor: add anchor scrolling + 100vh banner

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -39,7 +39,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      anchorScrolling: 'enabled',
+      onSameUrlNavigation: 'reload',
+      scrollPositionRestoration: 'enabled'
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
