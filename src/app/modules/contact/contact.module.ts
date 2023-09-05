ea2c5cca514344f19b4135ec8a9cb052afde0a79
eliascmurat <elias-murat@outlook.com>
2023-09-04 21:17:45 -0300

    feat: create form page

--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -3,14 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     ContactComponent
   ],
-    imports: [
-        CommonModule,
-        ContactRoutingModule,
-    ]
+  exports: [
+    ContactComponent
+  ],
+  imports: [
+    CommonModule,
+    ContactRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
+  ]
 })
 export class ContactModule { }
