b0fbb79a9ce824859ac1fcdb4ad4223d61192236
eliascmurat <elias-murat@outlook.com>
2023-09-05 21:40:14 -0300

    feat: create not found page + add imgs + improving css

--- src/app/modules/not-found/not-found.component.ts
+++ src/app/modules/not-found/not-found.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.redirectWithFragment();
+  }
+
+  redirectWithFragment(): void {
+    const fragment = 'ops';
+    window.location.href = '/not-found#' + fragment;
+  }
 
 }
--- src/app/modules/not-found/not-found.module.ts
+++ src/app/modules/not-found/not-found.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { NotFoundRoutingModule } from './not-found-routing.module';
 import { NotFoundComponent } from './not-found.component';
+import {RouterModule} from "@angular/router";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 @NgModule({
   declarations: [
     NotFoundComponent
   ],
-    imports: [
-        CommonModule,
-        NotFoundRoutingModule,
-    ]
+  imports: [
+    CommonModule,
+    NotFoundRoutingModule,
+    RouterModule,
+    FontAwesomeModule
+  ]
 })
 export class NotFoundModule { }
